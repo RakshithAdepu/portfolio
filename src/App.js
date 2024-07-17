a7148063f8a36e238bb24586ed736657a93fdcef
Rakshith Adepu <rakshithadepu09@gmail.com>
2024-07-17 15:58:03 +0530

    update a few changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-
 import React from 'react';
-import {BrowserRouter, Route, Routes, } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
-import Portfolio  from "./components/Portfolio/Portfolio";
+import Portfolio from './components/Portfolio/Portfolio';
 import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 
-
-const App=()=>{
-
-  return(
+const App = () => {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path="/"  element={<Portfolio/>}/>
-      <Route exact path="/About"  element={<About/>}/>
-      <Route exact path="/Skills"  element={<Skills/>}/>
-      <Route exact path="/Contact"  element={<Contact/>}/>
-    </Routes>
+      <Helmet>
+      <link rel="icon" href="https://res.cloudinary.com/dcbgett3c/image/upload/v1684772677/Picsart_23-05-22_20-02-18-395_bfmqsh.jpg" sizes="any" />
+  <title>Rakshith Adepu</title>
+      </Helmet>
+      <Routes>
+        <Route exact path="/" element={<Portfolio />} />
+        <Route exact path="/About" element={<About />} />
+        <Route exact path="/Skills" element={<Skills />} />
+        <Route exact path="/Contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
-
+  );
+};
 
 export default App;
